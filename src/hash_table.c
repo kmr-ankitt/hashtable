6c2aaecf4b38cc895cdf73c159e15f905f43a8d2
ankit <sahuankit.murhu@gmail.com>
2026-04-06 11:22:36 +0530

    chore: formatting

--- src/hash_table.c
+++ src/hash_table.c
@@ -1,4 +1,5 @@
 #include "hash_table.h"
+
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
@@ -42,8 +43,7 @@ static void ht_del_item(ht_item *item) {
 void ht_del_hash_table(ht_hash_table *ht) {
   for (int i = 0; i < ht->size; i++) {
     ht_item *item = ht->items[i];
-    if (item != NULL)
-      ht_del_item(item);
+    if (item != NULL) ht_del_item(item);
   }
 
   free(ht->items);
