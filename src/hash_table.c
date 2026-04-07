fc7285c5dd5383284224873fd25a682bab03b804
ankit <sahuankit.murhu@gmail.com>
2026-04-07 22:44:53 +0530

    feat: insert functionlity in hashmap

--- src/hash_table.c
+++ src/hash_table.c
@@ -86,3 +86,28 @@ int ht_get_hash(const char *s, const int num_buckets, const int attempt) {
 
   return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
 }
+
+/**
+ * ht_insert - inserts a key-value pair into the hash table
+ *
+ * - creates a new item with the given key and value
+ * - calculates the index for that item
+ * - if there is a collision, it will use double hashing
+ * - else insert the item into the hash table
+ **/
+
+void ht_insert(ht_hash_table *ht, const char *key, const char *value) {
+  ht_item *item = ht_new_item(key, value);
+  int index = ht_get_hash(item->key, ht->size, 0);
+  ht_item *cur_item = ht->items[index];
+  int i = 1;
+
+  while (cur_item != NULL) {
+    index = ht_get_hash(item->key, ht->size, i);
+    cur_item = ht->items[index];
+    i++;
+  }
+
+  ht->items[index] = item;
+  ht->count++;
+}
